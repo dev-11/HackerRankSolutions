d7e9122600616e6c1976c9a68359327debf48a58
Otto <giotto007@gmail.com>
2019-02-19 14:27:48 +0000

    folder change

--- Algorithms/Greedy/SherlockAndTheBeast/solution.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    start_number=int(input())
-    decent_number=start_number
-    
-    while(decent_number%3!=0):
-        decent_number-=5
-    
-    print('-1' if decent_number<0 else decent_number*'5'+(start_number-decent_number)*'3')
-
